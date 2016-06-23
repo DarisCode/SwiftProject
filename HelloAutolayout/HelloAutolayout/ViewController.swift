f4fa7cb1d0059d39ace3539eb00f1e02e9b3abc0
Financialbrain <evolutionkuei@gmail.com>
2016-06-23 21:33:43 +0800

    Autolayoutt

---
+++ HelloAutolayout/HelloAutolayout/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HelloAutolayout
+//
+//  Created by Financialbrain on 2016/6/23.
+//  Copyright © 2016年 DarisCode. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
